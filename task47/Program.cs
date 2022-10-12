47e4407c9fc89c7d33e314edd0965ebef3acad75
tarana <beshirova@hotmail.com>
2022-10-12 20:28:22 +0300

    task47

--- task47/Program.cs
+++ task47/Program.cs
@@ -12,12 +12,12 @@ int n = int.Parse(Console.ReadLine());
 Random rnd = new Random();
 double [,] array = new double [m,n];
 
-for (int i = 0; i < m; i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-     for (int j = 0; j < n; j++)
+     for (int j = 0; j < array.GetLength(1); j++)
 { 
     array[i,j]=rnd.Next(-100,100)/10.0;
-Console.Write(array[i,j] ); 
+Console.Write($"{array[i,j]}  "); 
 } 
 Console.WriteLine(); 
 } 
